58f237377743f5aa7433b1178aea0927a56ecd19
NyoronDev <nyoronsheppard@outlook.es>
2020-11-20 16:24:13 +0100

    5 - Singleton

---
+++ Singleton/AmbientContext/Building.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Singleton.AmbientContext
+{
+    public class Building
+    {
+        public List<Wall> Walls { get; set; } = new List<Wall>();
+    }
+}
---
+++ Singleton/SingletonDependencyInjection/IDatabase.cs
@@ -0,0 +1,7 @@
+﻿namespace Singleton.SingletonDependencyInjection
+{
+    public interface IDatabase
+    {
+        int GetPopulation(string name);
+    }
+}
