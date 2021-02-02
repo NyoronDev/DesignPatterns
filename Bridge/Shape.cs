7c0eb38ccbdb18e51f0225b8c93d6b8d58eb5cf9
NyoronDev <nyoronsheppard@outlook.es>
2021-02-02 18:44:29 +0100

    Bridge

---
+++ Bridge/Shape.cs
@@ -0,0 +1,16 @@
+﻿namespace Bridge
+{
+    public abstract class Shape
+    {
+        protected IRenderer renderer;
+
+        protected Shape(IRenderer renderer)
+        {
+            this.renderer = renderer;
+        }
+
+        public abstract void Draw();
+
+        public abstract void Resize(float factor);
+    }
+}
