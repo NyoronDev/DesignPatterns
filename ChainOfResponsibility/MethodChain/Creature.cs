dc2c9290de01f02c6367d462c78bce9bd7d016c2
NyoronDev <nyoronsheppard@outlook.es>
2021-02-08 09:04:59 +0100

    Chain of responsibility

---
+++ ChainOfResponsibility/MethodChain/Creature.cs
@@ -0,0 +1,21 @@
+﻿namespace ChainOfResponsibility.MethodChain
+{
+    public class Creature
+    {
+        public string Name { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+
+        public Creature(string name, int attack, int defense)
+        {
+            Defense = defense;
+            Attack = attack;
+            Defense = defense;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Attack} - {Defense}";
+        }
+    }
+}
