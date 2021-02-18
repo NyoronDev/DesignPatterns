932370273dc3d0dfcf048132ad8150423dc1547e
NyoronDev <nyoronsheppard@outlook.es>
2021-02-18 09:25:06 +0100

    Additional Content

---
+++ AdditionalContent/LocalInversionOfControl/Person.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace AdditionalContent.LocalInversionOfControl
+{
+    public class Person
+    {
+        public List<string> Names = new List<string>();
+        public List<Person> Children = new List<Person>();
+    }
+}
